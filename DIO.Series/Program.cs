e254db258a4979e1b3b06fdafe4489496ce86967
clovisdanielcosta <clovisdanielcosta@gmail.com>
2021-09-01 20:13:04 -0300

    Created Folders Structure and Classes EntidadeBase, Serie e Genero

---
+++ DIO.Series/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DIO.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serie meuObjeto = new Serie();
+       
+
+            Console.WriteLine("Hello World!"); 
+        }
+    }
+}
